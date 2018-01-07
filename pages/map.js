d32b1265d6e19bbf2a7fc70ce15e6047a2aa5e00
Andrew <andrew.cherniavskii@gmail.com>
2018-01-07 16:55:49 +0100

    add leaflet map

--- pages/map.js
+++ pages/map.js
@@ -2,11 +2,22 @@ import React from 'react';
 import Typography from 'material-ui/Typography';
 import withRoot from '../src/withRoot';
 import Link from '../src/components/link';
+import lStyles from 'leaflet/dist/leaflet.css';
 
 class MapPage extends React.Component {
+  componentDidMount() {
+    const { Map } = require('leaflet');
+    // require('leaflet/dist/leaflet.css');
+    this.map = new Map('map').setView([51.505, -0.09], 13);
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
   render() {
     return (
       <div>
+        <style jsx global>{lStyles}</style>
+        <div id="map" style={{ width: '100%', height: '500px' }} />
         <Typography>Map page</Typography>
         <Link href="/">
           Index page
